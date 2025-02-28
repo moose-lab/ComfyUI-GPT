08a9fc37a49cb467d816062e4c5a97f007256130
moose-lab <mooseindiehacker@gmail.com>
2025-02-28 10:32:48 +0800

    feat: initialize registeration 'comfyui-gpt' to ComfyUI standard method

---
+++ __init__.py
@@ -0,0 +1,10 @@
+
+# ComfyUI-GPT 前端注册到web服务器
+WEB_DIRECTORY = "web"
+
+
+# ComfyUI-GPT 后端注册到custom nodes
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
